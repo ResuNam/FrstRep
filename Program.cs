65a9797403c02c68d244fce2eeb89b077f7e08cf
ResuNam <stud.alesha@gmail.com>
2022-03-24 23:00:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BarsEv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Press_c_to_exit();
+            obj.OnKeyPressed += (object sender, char c) => Console.WriteLine(c);
+            obj.Run();
+        }
+    }
+
+    class Press_c_to_exit
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            ConsoleKeyInfo c;
+            do
+            {
+                c = Console.ReadKey();
+                if (c.Key == ConsoleKey.C)
+                    break;
+                OnKeyPressed?.Invoke(this, c.KeyChar);
+
+            } while (true);
+        }
+    }
+}

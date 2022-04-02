e197ef4fc5218c58fee9a93443bb2af18fce3110
ResuNam <stud.alesha@gmail.com>
2022-04-03 00:34:13 +0300

    IEnumerable, Task 2 — Collections

---
+++ IEnumerable_2_Collections/Task_2/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    class Entity
+    {
+        public int Id;
+        public int ParentId;
+        public string Name;
+
+        public Entity( int id, int pId, string name)
+        {
+            Id = id;
+            ParentId = pId;
+            Name = name;
+        }
+    }
+}
---
+++ IEnumerable_2_Collections/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Entity> 
+            {
+                new Entity(1, 0, "Root entity"),
+                new Entity(2, 1, "Child of 1 entity"),
+                new Entity(3, 1, "Child of 1 entity"),
+                new Entity(4, 2, "Child of 2 entity"),
+                new Entity(5, 4, "Child of 4 entity"),
+            };
+
+            var d = CreateDictionary(list);
+            foreach (var item in d)
+            {
+                Console.Write($"Key = {item.Key}, Value = [ ");
+                foreach (var child in item.Value)
+                    Console.Write(' ' + child.Id.ToString());
+                Console.WriteLine(" ]");
+            }
+        }
+        public static Dictionary<int, List<Entity>> CreateDictionary(List<Entity> entities)
+        {
+            var dictionary = new Dictionary<int, List<Entity>>();
+            foreach (var e in entities)
+            {
+                dictionary.TryAdd(e.ParentId, new List<Entity>());
+                dictionary[e.ParentId].Add(e);
+            }
+            return dictionary;
+        }
+    }
+}
